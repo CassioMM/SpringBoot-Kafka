3326427d814726b2b805608eae1ce021d709f181
Cássio Montenegro <tatcho156@gmail.com>
2026-04-22 23:58:28 -0300

    Create Kafka Producer to Produce JSON Message

--- src/main/java/net/cassiom/springboot_kafka/SpringbootKafkaApplication.java
+++ src/main/java/net/cassiom/springboot_kafka/SpringbootKafkaApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbootKafkaApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(SpringbootKafkaApplication.class, args);
 	}
 
